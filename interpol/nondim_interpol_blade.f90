1782c4c1a1a8d34aef208baf2c5fcee0a32781db
Luke Battey <lukebattey@gmail.com>
2017-02-01 17:27:08 -0500

    tried it and it didn't increment il

--- interpol/nondim_interpol_blade.f90
+++ interpol/nondim_interpol_blade.f90
@@ -6,11 +6,17 @@ PROGRAM nondim_interpol_blade
 !------------------------------------------------------------
 
 INTEGER,PARAMETER::rDef=SELECTED_REAL_KIND(12)
-
 INTEGER,PARAMETER:: imax=16
-INTEGER::i
-REAL(KIND=rDef),DIMENSION(1:imax):: r, rR, rRi, c, cR, cRi, tw, twi
-REAL(KIND=rDef),PARAMETER:: Rmax = 5.03
+INTEGER::i,numrR,il
+REAL(KIND=rDef),DIMENSION(1:imax) :: r, rR, rRi, c, cR, cRi, tw, twi
+REAL(KIND=rDef),PARAMETER:: Rmax = 5.03, &
+							rRmin = 0.275, &
+							rRmax = 0.975
+REAL(KIND=rDef):: rRinc
+numrR = 15
+
+rRinc = (rRmax - rRmin)/(numrR - 1)
+
 
 OPEN(16,FILE = 'Blade_geo_tab.txt', FORM = 'FORMATTED')
 OPEN(26,FILE = 'Nondim_blade.txt', FORM = 'FORMATTED')
@@ -22,37 +28,29 @@ DO i=1,imax
 	WRITE(26,*) rR(i),cR(i)
 END DO
 
-iLower = 1
-
 ! make sure rR(1) is less than your first radial location (0.275)
 ! make sure rR(iMax) is greater than your last radial location (1)
 ! if not, print error and quit
+IF (rR(1)<rRmin .AND. rR(imax)>rRmax) THEN
+ELSE
+WRITE(6,*) 'ERROR ERROR ERROR'
+END IF
 
-DO i=1,15
-	rRi(i) = 0.275+0.05*(i-1)
-	! Increment iLower until rR(iLower)
-	DO WHILE (rRi(i) < rR(iLower))
-		iLower = iLower + 1
-	END DO
-	! Interpolate between rR(iLower) and rR(iLower + 1)
-!	WRITE(6,*) rR(i),cR(i)
-	WRITE(6,*) rRi(i)
-	IF (rR(i) <= rRi(i) .AND. rR(i+1) >= rRi(i)) THEN
-		cRi(i) = cR(i) + ((cR(i+1)-cR(i))/(rR(i+1)-rR(i)))*(rRi(i)-rR(i))
-	
-	!ELSE IF ()
+!go to 100
 
-	ELSE 
-!	WRITE(6,*) "FUUUUUUUCK"
-
-	END IF
-	!WRITE(6,*) rRi(i)!,cRi(i)
+DO i=1,numrR
+    rRi(i) = rRmin + rRinc*(i*1.0-1.0)
+!WRITE(6,*) rRi(i)
+! Increment il until rR(iLower)
+	    DO WHILE (rRi(i) < rR(il))
+        il = il + 1
+	    END DO 
+	    WRITE(6,*) il
+	! Interpolate between rR(il) and rR(il + 1)
+	!	cRi(i) = cR(i) + ((cR(i+1)-cR(i))/(rR(il+1)-rR(il)))*(rRi(i)-rR(il))
+	!	WRITE(6,*) rRi(i),cRi(i)
 END DO
 
-
-
-
-
-
+!100 continue
 
 END PROGRAM nondim_interpol_blade

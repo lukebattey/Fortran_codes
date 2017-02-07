35b693c62a3eed37668013bb32f5c1f17a753a6f
Luke Battey <lukebattey@gmail.com>
2017-02-06 22:20:31 -0500

    added inputfile.dat into elliptic_grid_gen.f90, still needs some work but it's there now

--- gridgen/elliptic_grid_gen.f90
+++ gridgen/elliptic_grid_gen.f90
@@ -1,5 +1,5 @@
 PROGRAM elliptic_grid_gen
-	IMPLICIT NONE
+    IMPLICIT NONE
 !
 !----------------------------Variables ---------------------------------------------------
 INTEGER,PARAMETER::rDef=SELECTED_REAL_KIND(12)
@@ -11,17 +11,21 @@ REAL(KIND=rDef),ALLOCATABLE,DIMENSION(:,:,:) :: X,Xsi,Xsisi,Xeta,Xetaeta, &
                                                 bby,ddy,aay,ccy,xResid,yResid
 
 REAL(KIND=rDef),ALLOCATABLE,DIMENSION(:,:) :: psi,phi
-REAL(KIND=rDef),ALLOCATABLE,DIMENSION(:) :: RMSres,Sres
-
-
+REAL(KIND=rDef),ALLOCATABLE,DIMENSION(:) :: RMSres,Sres,cRMSr
+CHARACTER(len=30) :: infile,outfile
+LOGICAL :: srctrms
 !----------------------- Open files and set stipulations -----------------------------------------
 
-OPEN(26,FILE = 'clust_alg_grid.xy', FORM = 'FORMATTED')
+OPEN(16,FILE = 'inputfile.dat', FORM = 'FORMATTED')
+READ(16,*) infile 
+READ(16,*) outfile
+READ(16,*) srctrms
+READ(16,*) nmax
+READ(16,*) cRMSr
+
+OPEN(26,FILE = infile, FORM = 'FORMATTED')
 
 READ(26,*) imax,jmax
-nmax = 60 ! assumes n wont exceed nmax... 
-
-! ^ IS THERE A BETTER WAY TO DO THIS?????? If it matters or if there arent more pressing issues
 
 ALLOCATE(X(imax,jmax,nmax+1),Xsi(imax,jmax,nmax),Xsisi(imax,jmax,nmax),Xeta(imax,jmax,nmax), &
          Xetaeta(imax,jmax,nmax), &
@@ -36,7 +40,7 @@ ALLOCATE(RMSres(nmax),Sres(nmax))
 
 n = 1                                                            
 DO j=1,jmax                                                      
-	DO i=1,imax                                                  
+    DO i=1,imax                                                  
         READ(26,*) X(i,j,n),Y(i,j,n)
     END DO 
 END DO
@@ -46,7 +50,7 @@ CLOSE(26)
 n = 1
 DO j=1,jmax,(jmax-1)
     DO i=2,imax-1
-   		IF (ABS(X(i+1,j,n)-X(i-1,j,n)) > ABS(Y(i+1,j,n)-Y(i-1,j,n))) THEN
+        IF (ABS(X(i+1,j,n)-X(i-1,j,n)) > ABS(Y(i+1,j,n)-Y(i-1,j,n))) THEN
             phi(i,j) = -2*(X(i+1,j,n)-2*X(i,j,n)+X(i-1,j,n))/(X(i+1,j,n)-X(i-1,j,n))
         ELSE
             phi(i,j) = 2*(Y(i+1,j,n)-2*Y(i,j,n)+Y(i-1,j,n))/(Y(i+1,j,n)-Y(i-1,j,n))
@@ -68,124 +72,122 @@ DO j=2,jmax-1
     DO i=2,imax
         phi(i,j) = phi(i,1)+((j-1.0)/(jmax-1.0))*(phi(i,jmax)-phi(i,1))
         psi(i,j) = psi(1,j)+((i-1.0)/(imax-1.0))*(psi(imax,j)-psi(1,j))
-   END DO
+    END DO
 END DO
 
 !--------------------------------------------------------------------------------------------------------
 !======================= MAIN LOOP STARTS HERE ==========================================================
 !--------------------------------------------------------------------------------------------------------
 DO n = 1,nmax
-!----------------------- set boundaries to be constant -----------------------------------
-	DO i=1,imax
-    X(i,1,n+1) = X(i,1,1)               
-    X(i,jmax,n+1) = X(i,jmax,1)           
-    Y(i,1,n+1) = Y(i,1,1)
-    Y(i,jmax,n+1) = Y(i,jmax,1)          
+    !----------------------- set boundaries to be constant -----------------------------------
+    DO i=1,imax
+        X(i,1,n+1) = X(i,1,1)               
+        X(i,jmax,n+1) = X(i,jmax,1)           
+        Y(i,1,n+1) = Y(i,1,1)
+        Y(i,jmax,n+1) = Y(i,jmax,1)          
     END DO 
 
     DO j=1,jmax
-    X(1,j,n+1) = X(1,j,1)
-    X(imax,j,n+1) = X(imax,j,1)
-    Y(1,j,n+1) = Y(1,j,1)
-    Y(imax,j,n+1) = Y(imax,j,1)
+        X(1,j,n+1) = X(1,j,1)
+        X(imax,j,n+1) = X(imax,j,1)
+        Y(1,j,n+1) = Y(1,j,1)
+        Y(imax,j,n+1) = Y(imax,j,1)
     END DO
 
-DO j=2,jmax-1
-!-------------------------- get alpha, beta, and gama ---------------------------------------
-DO i=2,imax-1
-    Xeta(i,j,n) = (X(i,j+1,n)-X(i,j-1,n))/2
-    Yeta(i,j,n) = (Y(i,j+1,n)-Y(i,j-1,n))/2
-    alpha(i,j,n) = ((Xeta(i,j,n))**2)+((Yeta(i,j,n))**2)
+    DO j=2,jmax-1
+        !-------------------------- get alpha, beta, and gama ---------------------------------------
+        DO i=2,imax-1
+            Xeta(i,j,n) = (X(i,j+1,n)-X(i,j-1,n))/2
+            Yeta(i,j,n) = (Y(i,j+1,n)-Y(i,j-1,n))/2
+            alpha(i,j,n) = ((Xeta(i,j,n))**2)+((Yeta(i,j,n))**2)
 
-    Xsi(i,j,n) = (X(i+1,j,n)-X(i-1,j,n))/2
-    Ysi(i,j,n) = (Y(i+1,j,n)-Y(i-1,j,n))/2 
-    gama(i,j,n) = ((Xsi(i,j,n))**2)+((Ysi(i,j,n))**2)
-    
-    beta(i,j,n) = Xsi(i,j,n)*Xeta(i,j,n) + Ysi(i,j,n)*Yeta(i,j,n)
+            Xsi(i,j,n) = (X(i+1,j,n)-X(i-1,j,n))/2
+            Ysi(i,j,n) = (Y(i+1,j,n)-Y(i-1,j,n))/2 
+            gama(i,j,n) = ((Xsi(i,j,n))**2)+((Ysi(i,j,n))**2)
 
-!-------------------Finding Thomas array values --------------------------------------------
-    p = 2
-    IF (p == 1) THEN   ! <------------ Make this "IF (source terms == FALSE) ...."
-      bbx(i,j,n) = alpha(i,j,n)
-      aax(i,j,n) = bbx(i,j,n) 
-      ccx(i,j,n) = beta(i,j,n)*(X(i+1,j+1,n)-X(i+1,j-1,n+1)-X(i-1,j+1,n)+X(i-1,j-1,n+1))/2 &
-                   -gama(i,j,n)*(X(i,j+1,n)+X(i,j-1,n+1))
-        
-      bby(i,j,n) = alpha(i,j,n)
-      aay(i,j,n) = bby(i,j,n) 
-      ccy(i,j,n) = beta(i,j,n)*(Y(i+1,j+1,n)-Y(i+1,j-1,n+1)-Y(i-1,j+1,n)+Y(i-1,j-1,n+1))/2 &
-                     -gama(i,j,n)*(Y(i,j+1,n)+Y(i,j-1,n+1))
-    ELSE
-      bbx(i,j,n) = alpha(i,j,n)*(1 - phi(i,j)/2)
-   !   WRITE(6,*) i,j,n,bbx(i,j,n)
-      aax(i,j,n) = alpha(i,j,n)*(1 + phi(i,j)/2)
-      ccx(i,j,n) = beta(i,j,n)*(X(i+1,j+1,n)-X(i+1,j-1,n+1)-X(i-1,j+1,n)+X(i-1,j-1,n+1))/2 &
-      -gama(i,j,n)*(X(i,j+1,n)+X(i,j-1,n+1))-gama(i,j,n)*psi(i,j)*(X(i,j+1,n)-X(i,j-1,n+1))/2
+            beta(i,j,n) = Xsi(i,j,n)*Xeta(i,j,n) + Ysi(i,j,n)*Yeta(i,j,n)
 
-      bby(i,j,n) = alpha(i,j,n)*(1 - phi(i,j)/2)
-      aay(i,j,n) = alpha(i,j,n)*(1 + phi(i,j)/2)
-      ccy(i,j,n) = beta(i,j,n)*(Y(i+1,j+1,n)-Y(i+1,j-1,n+1)-Y(i-1,j+1,n)+Y(i-1,j-1,n+1))/2 &
-      -gama(i,j,n)*(Y(i,j+1,n)+Y(i,j-1,n+1))-gama(i,j,n)*psi(i,j)*(Y(i,j+1,n)-Y(i,j-1,n+1))/2
+            !-------------------Finding Thomas array values --------------------------------------------
+            IF (srctrms .eqv. .FALSE.) THEN   
+              bbx(i,j,n) = alpha(i,j,n)
+              aax(i,j,n) = bbx(i,j,n) 
+              ccx(i,j,n) = beta(i,j,n)*(X(i+1,j+1,n)-X(i+1,j-1,n+1)-X(i-1,j+1,n)+X(i-1,j-1,n+1))/2 &
+              -gama(i,j,n)*(X(i,j+1,n)+X(i,j-1,n+1))
 
-    END IF
-      ddy(i,j,n) = (-2)*(alpha(i,j,n) + gama(i,j,n))
-      ddx(i,j,n) = (-2)*(alpha(i,j,n) + gama(i,j,n))
+              bby(i,j,n) = alpha(i,j,n)
+              aay(i,j,n) = bby(i,j,n) 
+              ccy(i,j,n) = beta(i,j,n)*(Y(i+1,j+1,n)-Y(i+1,j-1,n+1)-Y(i-1,j+1,n)+Y(i-1,j-1,n+1))/2 &
+              -gama(i,j,n)*(Y(i,j+1,n)+Y(i,j-1,n+1))
+          ELSE
+              bbx(i,j,n) = alpha(i,j,n)*(1 - phi(i,j)/2)
 
-!------------------------ Find innitial Residuals -------------------------------------------
-  IF (n == 1) THEN
-  xResid(i,j,n) = bbx(i,j,n)*X(i-1,j,n)+ddx(i,j,n)*X(i,j,n)+aax(i,j,n)*X(i+1,j,n)-ccx(i,j,n)
-  yResid(i,j,n) = bby(i,j,n)*Y(i-1,j,n)+ddy(i,j,n)*Y(i,j,n)+aay(i,j,n)*Y(i+1,j,n)-ccy(i,j,n)
-  END IF
-END DO
+              aax(i,j,n) = alpha(i,j,n)*(1 + phi(i,j)/2)
+              ccx(i,j,n) = beta(i,j,n)*(X(i+1,j+1,n)-X(i+1,j-1,n+1)-X(i-1,j+1,n)+X(i-1,j-1,n+1))/2 &
+              -gama(i,j,n)*(X(i,j+1,n)+X(i,j-1,n+1))-gama(i,j,n)*psi(i,j)*(X(i,j+1,n)-X(i,j-1,n+1))/2
 
-!----------------- Setting Thomas array boundaries -----------------------------------------
-bbx(1,j,n) = 0
-bbx(imax,j,n) = 0
-ddx(1,j,n) = 1
-ddx(imax,j,n) = 1        ! X array boundaries
-aax(1,j,n) = 0
-aax(imax,j,n) = 0
-ccx(1,j,n) = x(1,j,n)
-ccx(imax,j,n) = x(imax,j,n)
+              bby(i,j,n) = alpha(i,j,n)*(1 - phi(i,j)/2)
+              aay(i,j,n) = alpha(i,j,n)*(1 + phi(i,j)/2)
+              ccy(i,j,n) = beta(i,j,n)*(Y(i+1,j+1,n)-Y(i+1,j-1,n+1)-Y(i-1,j+1,n)+Y(i-1,j-1,n+1))/2 &
+              -gama(i,j,n)*(Y(i,j+1,n)+Y(i,j-1,n+1))-gama(i,j,n)*psi(i,j)*(Y(i,j+1,n)-Y(i,j-1,n+1))/2
 
-bby(1,j,n) = 0
-bby(imax,j,n) = 0
-ddy(1,j,n) = 1
-ddy(imax,j,n) = 1        ! Y array boundaries
-aay(1,j,n) = 0
-aay(imax,j,n) = 0
-ccy(1,j,n) = y(1,j,n)
-ccy(imax,j,n) = y(imax,j,n)
+          END IF
+          ddy(i,j,n) = (-2)*(alpha(i,j,n) + gama(i,j,n))
+          ddx(i,j,n) = (-2)*(alpha(i,j,n) + gama(i,j,n))
 
-!----------------------- Thomas algorithm --------------------------------------
+          !------------------------ Find innitial Residuals -------------------------------------------
+          IF (n == 1) THEN
+              xResid(i,j,n) = bbx(i,j,n)*X(i-1,j,n)+ddx(i,j,n)*X(i,j,n)+aax(i,j,n)*X(i+1,j,n)-ccx(i,j,n)
+              yResid(i,j,n) = bby(i,j,n)*Y(i-1,j,n)+ddy(i,j,n)*Y(i,j,n)+aay(i,j,n)*Y(i+1,j,n)-ccy(i,j,n)
+          END IF
+      END DO
 
-	DO i=2,imax-1
+      !----------------- Setting Thomas array boundaries -----------------------------------------
+      bbx(1,j,n) = 0
+      bbx(imax,j,n) = 0
+      ddx(1,j,n) = 1
+      ddx(imax,j,n) = 1        ! X array boundaries
+      aax(1,j,n) = 0
+      aax(imax,j,n) = 0
+      ccx(1,j,n) = x(1,j,n)
+      ccx(imax,j,n) = x(imax,j,n)
+
+      bby(1,j,n) = 0
+      bby(imax,j,n) = 0
+      ddy(1,j,n) = 1
+      ddy(imax,j,n) = 1        ! Y array boundaries
+      aay(1,j,n) = 0
+      aay(imax,j,n) = 0
+      ccy(1,j,n) = y(1,j,n)
+      ccy(imax,j,n) = y(imax,j,n)
+
+      !----------------------- Thomas algorithm --------------------------------------
+      DO i=2,imax-1
         ddx(i,j,n) = ddx(i,j,n) - (bbx(i,j,n)/ddx(i-1,j,n))*aax(i-1,j,n)  ! X forward sweep
         ccx(i,j,n) = ccx(i,j,n) - (bbx(i,j,n)/ddx(i-1,j,n))*ccx(i-1,j,n)
 
         ddy(i,j,n) = ddy(i,j,n) - (bby(i,j,n)/ddy(i-1,j,n))*aay(i-1,j,n)  ! Y forward sweep
         ccy(i,j,n) = ccy(i,j,n) - (bby(i,j,n)/ddy(i-1,j,n))*ccy(i-1,j,n)
-	END DO
+      END DO
 
     ccx(imax,j,n) = ccx(imax,j,n)/ddx(imax,j,n)  
     ccy(imax,j,n) = ccy(imax,j,n)/ddy(imax,j,n)  
 
-	DO i=imax,2,-1
+    DO i=imax,2,-1
         ccx(i,j,n) = (ccx(i,j,n) - aax(i,j,n)*ccx(i+1,j,n))/ddx(i,j,n)   ! X forward sweep
         X(i,j,n+1) = ccx(i,j,n)
 
         ccy(i,j,n) = (ccy(i,j,n) - aay(i,j,n)*ccy(i+1,j,n))/ddy(i,j,n)   ! Y forward sweep
         Y(i,j,n+1) = ccy(i,j,n)
-	END DO
+    END DO
 
-!----------------------- Find Residual and its sum of squares ------------------------------------
-Sres(n) = 0
-DO i = 2,imax-1
-  xResid(i,j,n+1) = bbx(i,j,n)*X(i-1,j,n)+ddx(i,j,n)*X(i,j,n)+aax(i,j,n)*X(i+1,j,n)-ccx(i,j,n)
-  yResid(i,j,n+1) = bby(i,j,n)*Y(i-1,j,n)+ddy(i,j,n)*Y(i,j,n)+aay(i,j,n)*Y(i+1,j,n)-ccy(i,j,n)
+    !----------------------- Find Residual and its sum of squares ------------------------------------
+    Sres(n) = 0
+    DO i = 2,imax-1
+      xResid(i,j,n+1) = bbx(i,j,n)*X(i-1,j,n)+ddx(i,j,n)*X(i,j,n)+aax(i,j,n)*X(i+1,j,n)-ccx(i,j,n)
+      yResid(i,j,n+1) = bby(i,j,n)*Y(i-1,j,n)+ddy(i,j,n)*Y(i,j,n)+aay(i,j,n)*Y(i+1,j,n)-ccy(i,j,n)
 
-  Sres(n) = Sres(n) + (xResid(i,j,n+1) - xResid(i,j,n))**2 + (yResid(i,j,n+1) - yResid(i,j,n))**2
-END DO
+      Sres(n) = Sres(n) + (xResid(i,j,n+1) - xResid(i,j,n))**2 + (yResid(i,j,n+1) - yResid(i,j,n))**2
+  END DO
 
 END DO !---->  j loop
 
@@ -194,7 +196,8 @@ WRITE(6,*) n,RMSres(n)
 
 END DO !---->  n loop (main)
 
-!------------------------- Writing Results for TecPlot360ex----------------------------------------
+!------------------------- Writing Results to file for TecPlot360ex----------------------------------------
+
 
 !DO j=1,jmax
  !   DO i=1,imax

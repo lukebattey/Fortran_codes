82d0c765ae121dec6f9452c0f11a2d4e882d9dba
Luke Battey <lukebattey@gmail.com>
2017-02-26 16:49:07 -0500

    NEWEST_gridgen.f90 is the latest that was created after I decided to change my convergence criteria

--- gridgen/alg_gridgen.f90
+++ gridgen/alg_gridgen.f90
@@ -1,22 +1,17 @@
 PROGRAM alg_gridgen
-	IMPLICIT NONE
-!
-!   This program writes 3 grids, the first and second grid are written excactly as specified in
-!   the "Project #1" handout. The first grid does not cluster the grid points in the y direction
-!   (Cy = 0.001) and is written to the "unclust_alg_grid.xy" output file. The second grid does 
-!   cluster in the only the y direction (Cy = 2.0) and is written to the      
+    IMPLICIT NONE     
 !
 !----------------------------Variables -----------------------------
 INTEGER,PARAMETER::rDef=SELECTED_REAL_KIND(10)
 INTEGER,PARAMETER:: imax=41,jmax=19,fi=11,di=31,ci=41
 INTEGER::i,j,g,F
-REAL(KIND=rDef),DIMENSION(1:imax,1:jmax):: x,y
+REAL(KIND=rDef),DIMENSION(1:imax,1:jmax):: x,y,Ja
 REAL(KIND=rDef),PARAMETER:: fx = -.8, &
                 dx = 1.00, &
                 cx = 1.80, &
                 t = 0.15, &
                 xint = 1.008930411365
-REAL(KIND=rDef):: cy
+REAL(KIND=rDef):: cy,Xeta,Xsi,Yeta,Ysi
 
 !----------------- Open files and set stipulations ---------------
 
@@ -64,21 +59,47 @@ END DO
 !----------------- WRITE INTERIOR X POINTS  ------------------------
 
 DO j=2,jmax-1
-	DO i=1,imax
+    DO i=1,imax
         x(i,j) = x(i,1) + ((j*1.0-1.0)/(jmax*1.0-1.0))*(x(i,jmax)-x(i,1))
         y(i,j) = y(i,1) &
         -(((y(i,jmax)-y(i,1))/cy))*log(1+(exp(-cy)-1)*((j*1.0-1.0)/(jmax*1.0-1.0)))
     END DO
 END DO
 
-WRITE(F,'(A)') 'VARIABLES = "X" "Y" '
-WRITE(F,'(A,I3,A,I3)') 'ZONE I= ',imax,'    J=  ',jmax
-DO j=1,jmax
-	DO i=1,imax
-        WRITE(F,*) x(i,j),y(i,j)
-	END DO
+DO j=1,jmax  
+    DO i=1,imax
+        Xeta = (X(i,j+1)-X(i,j-1))/2
+        Yeta = (Y(i,j+1)-Y(i,j-1))/2
+        Xsi = (X(i+1,j)-X(i-1,j))/2
+        Ysi = (Y(i+1,j)-Y(i-1,j))/2
+        IF (j == jmax) THEN
+            Xeta = (3*X(i,j)-4*X(i,j-1)+X(i,j-2))/2
+            Yeta = (3*Y(i,j)-4*Y(i,j-1)+Y(i,j-2))/2
+        END IF
+        IF (j == 1) THEN
+             Xeta = -1*(3*X(i,j)-4*X(i,j+1)+X(i,j+2))/2
+             Yeta = -1*(3*Y(i,j)-4*Y(i,j+1)+Y(i,j+2))/2
+        END IF
+        IF (i == imax) THEN
+             Xsi = (3*X(i,j)-4*X(i-1,j)+X(i-2,j))/2
+             Ysi = (3*Y(i,j)-4*Y(i-1,j)+Y(i-2,j))/2
+        END IF
+        IF (i == 1) THEN
+             Xsi = -1*(3*X(i,j)-4*X(i+1,j)+X(i+2,j))/2
+             Ysi = -1*(3*Y(i,j)-4*Y(i+1,j)+Y(i+2,j))/2
+        END IF
+        Ja(i,j) = Xsi*Yeta - Xeta*Ysi
+    END DO
 END DO
-!----------------- WRITE ALL POINTS ------------------------
+
+WRITE(F,'(A)') 'VARIABLES = "X" "Y" "Ja"'
+WRITE(F,'(A,I3,A,I3)') 'ZONE I= ',imax,'    J=  ',jmax
+    DO j=1,jmax
+        DO i=1,imax
+            WRITE(F,*) x(i,j),y(i,j),Ja(i,j)
+        END DO
+    END DO
+    
 END DO
 
 
